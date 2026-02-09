2aac834fb637e614f7583586f12df270fdf17ca9
Mariame El dub <meldub@MacBook-Air-de-Mariame.local>
2026-02-10 00:04:13 +0100

    fix: __init__.py models - pas d'exécution auto

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,34 +1,31 @@
 """
-Package de Prédiction de Risque de Défaillance Industrielle
+Package de prédiction de défaillances industrielles.
 
-Ce package contient les modules nécessaires pour entraîner, évaluer et 
-utiliser un modèle de prédiction des risques de défaillance industrielle.
+Ce module contient les outils pour entraîner, évaluer et utiliser
+des modèles de prédiction de défaillances.
+
+Modules:
+- train_model: Entraînement multi-modèles
+- evaluation: Évaluation et métriques
+- predict_model: Prédictions sur nouvelles données
 """
 
-from train_model import train_and_evaluate
-from predict_model import predict
-from evaluation import evaluate_model
+from .train_model import ModelTrainer, train_pipeline
+from .evaluation import evaluate_model
+from .predict_model import load_model, predict
 
 __all__ = [
-    'train',
-    'predict',
+    'ModelTrainer',
+    'train_pipeline',
     'evaluate_model',
-    'calculate_metrics'
+    'load_model',
+    'predict'
 ]
 
-def model_train_and_evaluate():
-    trained_models, evaluation_results, model_paths, best_model = train_and_evaluate(data_path, target_column='failure_within_24h', models_to_train=None, 
-                      models_dir="models", test_size=0.2, random_state=42, cv=5)
+__version__ = '1.0.0'
+__author__ = 'Mariame El Dub'
+__project__ = 'Maintenance Prédictive Industrielle'
 
-
-featured_data = build_features(
-    input_dir=os.path.abspath("../../data/processed/augmented_data/"),
-    output_dir=os.path.abspath("../../data/processed/augmented_data/")
-)
-
-# Version du package
-__version__ = '0.1.0'
-
-# Informations sur le projet
-__project_name__ = 'Prédiction de Risque de Défaillance Industrielle'
-__author__ = 'Classe de Machine Learning'
+# Ne PAS exécuter automatiquement à l'import !
+# Pour utiliser:
+# from src.models import train_pipeline, evaluate_model, predict
